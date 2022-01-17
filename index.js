2c2c6d43351f76b091dca1018111c2fbd68d6a5a
Quentin.Boutet <boutet1406@gmail.com>
2022-01-17 22:02:56 +0100

    first commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let letters = window.document.getElementById('letters')
 
 
 const printLetters = () => {
-    let tab = ["J","'","e","t","'","a","i","m","e"];
+    let tab = [];
     let letters = window.document.getElementById('letters')
     for( i = 0; i < tab.length; i++ ){
         letters.innerHTML = tab[i];
